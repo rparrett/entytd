5569121cc361365fd3d80b305e30ee44404b92f0
Rob Parrett <robparrett@gmail.com>
2023-12-16 10:57:37 -0700

    Switch back to normal level

--- src/level.rs
+++ src/level.rs
@@ -35,7 +35,7 @@ fn queue_load(
     mut loading_assets: ResMut<LoadingAssets>,
     mut loading_resources: ResMut<LoadingResources>,
 ) {
-    let handle = asset_server.load("levels/stress.level.ron");
+    let handle = asset_server.load("levels/1.level.ron");
     loading_assets.0.push(handle.clone().into());
     commands.insert_resource(LevelHandle(handle));
     loading_resources.0 += 1;
